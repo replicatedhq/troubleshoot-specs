fd3fa9da0da5c6cc718d36e33f1af87e10f38487
AJ <aj@replicated.com>
2022-02-10 03:17:58 -0500

    Retab concat py

--- concat.py
+++ concat.py
@@ -10,20 +10,20 @@ with open('./analyzers.yaml') as analyzer:
 
 # Dict with the final spec
 spec_final = {
-	"apiVersion": "troubleshoot.sh/v1beta2",
-	"kind": "SupportBundle",
-	"metadata": {
-		"name": "concat-spec",
-	},
-	"spec": {
-		"collectors": {},
-		"analyzers": {},
-	},
+  "apiVersion": "troubleshoot.sh/v1beta2",
+  "kind": "SupportBundle",
+  "metadata": {
+    "name": "concat-spec",
+  },
+  "spec": {
+    "collectors": {},
+    "analyzers": {},
+  },
 }
 
 # Concat the Collectors and Analyzers specs
-spec_final["spec"]["collectors"] 	= data_collector["spec"]["collectors"]
-spec_final["spec"]["analyzers"] 	= data_analyzer["spec"]["analyzers"]
+spec_final["spec"]["collectors"]  = data_collector["spec"]["collectors"]
+spec_final["spec"]["analyzers"]   = data_analyzer["spec"]["analyzers"]
 
 # Write final spec file
 with open(r'support-bundle.yaml', 'w') as support_bundle_file:
